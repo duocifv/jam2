f23c485ccf05c9c64e4b63fab94e37f040b84685
Duoc <lytuongdn@gmail.com>
2024-10-22 22:12:13 +0700

    deploy 5

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -16,6 +16,12 @@ export const dynamicParams = false
 
 export async function generateStaticParams() {
   const response:any = await fetch("https://6717b3deb910c6a6e0298d04.mockapi.io/blog");
+
+  if (!response.ok) {
+    console.error('Lỗi khi lấy dữ liệu:', response.statusText);
+    return []; // Trả về mảng rỗng nếu có lỗi
+  }
+
   const posts = await response.json();
 
   const params = posts.map((post:any) => ({
@@ -33,8 +39,20 @@ export async function generateMetadata({
 }){
 
   const response = await fetch(`https://6717b3deb910c6a6e0298d04.mockapi.io/blog/${params.id}`);
-  const post:any = await response.json();
 
+  if (!response.ok) {
+    console.error('Lỗi khi lấy dữ liệu bài viết:', response.statusText);
+    return {
+      title: "Bài viết không tồn tại",
+      description: "Không có mô tả khả dụng",
+    };
+  }
+
+  const post:any = await response.json();
+  if (!response.ok) {
+    console.error('Lỗi khi lấy dữ liệu:', response.statusText);
+    return <div>Lỗi khi lấy bài viết.</div>;
+  }
   return {
     title: post.name || "Post not found",
     description: post.name ? post.name : "No description available",
@@ -47,6 +65,10 @@ export default async function Page({
   params: { id: string };
 }) {
   const response:any = await fetch(`https://6717b3deb910c6a6e0298d04.mockapi.io/blog/${params.id}`);
+  if (!response.ok) {
+    console.error('Lỗi khi lấy dữ liệu bài viết:', response.statusText);
+    return <div>Lỗi khi lấy bài viết.</div>;
+  }
   const post:any = await response.json();
   return <div>
     <div>{post.name}</div>
