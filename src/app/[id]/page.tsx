926bce83279c5a71e6c1cd17c1e741ed31e1a2de
duocifv <nvduocfpt@gmail.com>
2024-10-23 11:13:31 +0700

    deploy 231

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,52 +1,40 @@
+import Detail from "@/components/detail/Detail";
 import GraphQLQuery from "@/components/GraphQLQuery";
 import cache from "@/lib/graphqlClient";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { gql } from "graphql-request";
 import Image from "next/image";
 import React from "react";
 /* eslint-disable next-on-pages/no-app-nodejs-dynamic-ssg */
 
-const endpoint = process.env.NEXT_PUBLIC_API_URL
-export const runtime = 'edge';
+const endpoint = process.env.NEXT_PUBLIC_API_URL;
+
+//export const runtime = 'edge';
 
 export async function generateStaticParams() {
-  const response:any = await fetch(`${endpoint}/blog`);
-
-  if (!response.ok) {
-    console.error('Lỗi khi lấy dữ liệu:', response.statusText);
-    return []; // Trả về mảng rỗng nếu có lỗi
-  }
-
+  const response: any = await fetch(`${endpoint}/blog`);
+  if (!response.ok) return [];
   const posts = await response.json();
-
-  const params = posts.map((post:any) => ({
-    id: post.id.toString() // Chuyển đổi id thành chuỗi
+  const params = posts.map((post: any) => ({
+    id: post.id.toString(),
+    page: post.id.toString(),
   }));
 
-  return params; // Trả về danh sách các params cho các trang
+  return params;
 }
 
-
-export async function generateMetadata({
-  params,
-}: {
-  params: { id: string };
-}){
-
+export async function generateMetadata({ params }: { params: { id: string } }) {
   const response = await fetch(`${endpoint}/blog/${params.id}`);
-
-  if (!response.ok) {
-    console.error('Lỗi khi lấy dữ liệu bài viết:', response.statusText);
+  if (!response.ok)
     return {
       title: "Bài viết không tồn tại",
       description: "Không có mô tả khả dụng",
     };
-  }
-
-  const post:any = await response.json();
-  if (!response.ok) {
-    console.error('Lỗi khi lấy dữ liệu:', response.statusText);
-    return <div>Lỗi khi lấy bài viết.</div>;
-  }
+  const post: any = await response.json();
   return {
     title: post.name || "Post not found",
     description: post.name ? post.name : "No description available",
@@ -58,19 +46,20 @@ export default async function PageDetail({
 }: {
   params: { id: string };
 }) {
-  const response:any = await fetch(`${endpoint}/blog/${params.id}`);
-
-  if (!response.ok) {
-    console.error('Lỗi khi lấy dữ liệu bài viết:', response.statusText);
-    return <div>Lỗi khi lấy bài viết.</div>;
-  }
-  const post:any = await response.json();
-
-  const headers = new Headers();
-  headers.set('Cache-Control', 'public, max-age=60, stale-while-revalidate=30');
-
-  return <div>
-    <div>{post.name}</div>
-    <div>{post.createdAt}</div>
-  </div>;
+  const queryClient = new QueryClient();
+  const post = await queryClient.fetchQuery({
+    queryKey: ["blogs"],
+    queryFn: async () => {
+      const response = await fetch(`${endpoint}/blog`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch blog posts");
+      }
+      return response.json();
+    },
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Detail id={params.id}/>
+    </HydrationBoundary>
+  );
 }
--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -4,10 +4,10 @@ import { getRequestContext } from '@cloudflare/next-on-pages';
 export const runtime = 'edge';
 
 export async function GET(request: NextRequest) {
-  const ctx:any = getRequestContext();
-  const kv = ctx.env.posts;
-  await kv.put('exampleKey', 'This is a test value');
-  const value = await kv.get('exampleKey');
-  const responseText = `Hello World. Value from KV: ${value}`;
-  return new Response(responseText);
+  // const ctx:any = getRequestContext();
+  // const kv = ctx.env.posts;
+  // await kv.put('exampleKey', 'This is a test value');
+  // const value = await kv.get('exampleKey');
+  // const responseText = `Hello World. Value from KV: ${value}`;
+  return new Response("responseText");
 }
