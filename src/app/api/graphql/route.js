caa9197eb51a1429a382e0fcc94347e4fd312f05
Duoc <lytuongdn@gmail.com>
2024-10-23 20:02:27 +0700

    22222222222222

--- src/app/api/graphql/route.js
+++ src/app/api/graphql/route.js
@@ -35,5 +35,29 @@ export async function GET(req) {
 }
 
 export async function POST(req) {
-  return yoga.handle(req);
+  const url = new URL(req.url);
+  const queryParam = url.searchParams.get("query");
+  const cache = caches.default;
+  const params = queryParam;
+  const cacheKey = new Request(`https://products?${params.toString()}`).url;
+  const cachedResponse = await cache.match(cacheKey);
+  console.log("params", cacheKey, cachedResponse)
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+  const response = await yoga.handle(req);
+  const responseClone = response.clone();
+  await cache.put(cacheKey, responseClone, {
+    expirationTtl: 300,
+  });
+  const headers = new Headers(response.headers);
+  headers.set("Cache-Control", "public, max-age=30, stale-while-revalidate=10");
+  return new Response(response.body, {
+    status: response.status,
+    headers: headers,
+  });
 }
+
+// export async function POST(req) {
+//   return yoga.handle(req);
+// }
