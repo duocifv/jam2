694cdc6f8ed78a4800fc1dd1725feb482b635fb0
duocifv <nvduocfpt@gmail.com>
2024-10-23 17:00:12 +0700

    uppppppppp

--- src/app/api/graphql/route.js
+++ src/app/api/graphql/route.js
@@ -1,17 +1,37 @@
 // src/app/api/graphql/route.js
-import { createYoga } from 'graphql-yoga';
-import schema from '@/lib/schema';
+import { createYoga } from "graphql-yoga";
+import schema from "@/lib/schema";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 const yoga = createYoga({
   schema,
   playground: true,
-  graphqlEndpoint: '/api/graphql',
+  graphqlEndpoint: "/api/graphql",
 });
 
 export async function GET(req) {
-  return yoga.handle(req);
+  const url = new URL(req.url);
+  const queryParam = url.searchParams.get("query");
+  console.log("queryParam", queryParam);
+  const cache = caches.default;
+  const cacheKey = new Request(`https://products`).url;
+  const cachedResponse = await cache.match(cacheKey);
+  
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+  const response = await yoga.handle(req);
+  const responseClone = response.clone();
+  await cache.put(cacheKey, responseClone, {
+    expirationTtl: 300,
+  });
+  const headers = new Headers(response.headers);
+  headers.set("Cache-Control", "public, max-age=30, stale-while-revalidate=10");
+  return new Response(response.body, {
+    status: response.status,
+    headers: headers,
+  });
 }
 
 export async function POST(req) {
--- src/lib/schema.js
+++ src/lib/schema.js
@@ -15,37 +15,15 @@ const typeDefs = `
 const resolvers = {
   Query: {
     blogs: async () => {
-      const cache = caches.default; // Xác định cache
       const cacheKey = new Request("https://6717b3deb910c6a6e0298d04.mockapi.io/blog/");
-      const cachedResponse = await cache.match(cacheKey);
-      
-      // Kiểm tra cached response
-      if (!cachedResponse) {
-        console.error("No cached response found for:", cacheKey);
-      } else {
-        console.log("Found cached response:", cachedResponse);
-      }
-
-      // Nếu không tìm thấy cached response, gọi fetch
       const res = await fetch(cacheKey);
       if (!res.ok) {
         throw new Error("Lỗi khi lấy dữ liệu từ MockAPI");
       }
-
-      const data = await res.json(); // Lấy dữ liệu từ response
-
-      // Tạo Response mới cho cache
-      const newResponse = new Response(JSON.stringify(data), {
-        headers: { "Content-Type": "application/json" },
-      });
-
-      // Lưu Response vào cache
-      await cache.put(cacheKey, newResponse.clone());
-      return data; // Trả về dữ liệu
+      const data = await res.json();
+      return data; 
     },
   },
 };
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-module.exports = schema;
+module.exports = makeExecutableSchema({ typeDefs, resolvers });
