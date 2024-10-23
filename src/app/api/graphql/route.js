44f7b9782745d8d79d54f089ef36497d44b4cec9
Duoc <lytuongdn@gmail.com>
2024-10-23 21:18:12 +0700

    upload2222

--- src/app/api/graphql/route.js
+++ src/app/api/graphql/route.js
@@ -65,8 +65,6 @@ export async function POST(req) {
     headers: {
       "Content-Type": "application/json",
       "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-      "Pragma": "no-cache",
-      "Expires": "0",
     },
   });
 
